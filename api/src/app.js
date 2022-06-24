4dd1f2a1d29d5cd13a43dfccde9b4027f521b3c4
FdR-23 <federampi@gmail.com>
2022-06-24 00:49:50 -0300

    fixCors

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,9 @@ const cors = require('cors')
 server.name = 'API';
 
 
+
 server.use(cors())
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
