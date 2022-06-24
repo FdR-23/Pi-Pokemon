35a6b295c2ce596924887f7a9fc4a6cc2c55b954
FdR-23 <federampi@gmail.com>
2022-06-23 22:12:59 -0300

    prueba

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store'
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3005";
 
 ReactDOM.render(
   <Provider store={store}>
